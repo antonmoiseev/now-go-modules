84b93cee1e63d58082c6fba594e885addbcf73c9
Anton Moiseev <a@antonmoiseev.com>
2019-04-27 10:48:42 +0400

    Change module path

--- api/handlers/with_child.go
+++ api/handlers/with_child.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/antonmoiseev/now-go-modules/handlers/child"
+	"github.com/antonmoiseev/now-go-modules/api/handlers/child"
 )
 
 func Child(w http.ResponseWriter, r *http.Request) {
--- api/main.go
+++ api/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/antonmoiseev/now-go-modules/handlers"
+	"github.com/antonmoiseev/now-go-modules/api/handlers"
 )
 
 func main() {
